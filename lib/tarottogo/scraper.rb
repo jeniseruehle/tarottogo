695f7cad71620167e90da55f787f763f8663e65a
Jenise Ruehle <jela07@gmail.com>
2020-06-11 23:54:25 +0000

    updates

--- lib/tarottogo/scraper.rb
+++ lib/tarottogo/scraper.rb
@@ -10,9 +10,13 @@ class Tarottogo::Scraper
     cards = []
     get_page.css("div.grid.tarot-list.page").each do |cardset|
       cardset.css("grid__item large--one-quarter medium--one-third text-center card").each do |card|
-      name = card.css()
-  end
- 
+      card_name = card.css("h3").text
+      card_meaning = card.css("div.rte.rte--indented-images")
+    cards << {name: card_name, meaning: card_meaning}  
+    end
+  end 
+  cards 
+ end 
 
 end
 

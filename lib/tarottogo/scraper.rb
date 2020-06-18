69f9771c9094a996d0f24c895af42a253dda3fdd
Jenise Ruehle <jela07@gmail.com>
2020-06-18 14:34:11 +0000

    updates

--- lib/tarottogo/scraper.rb
+++ lib/tarottogo/scraper.rb
@@ -10,17 +10,16 @@ class Tarottogo::Scraper
   end 
   
   def scrape 
-    scrape_cards
+    scrape_cardsets
   end 
   
-  def scrape_cards(cardset_url)
+  def scrape_cardsets(cardset_url)
     cards = []
     html = Nokogiri::HTML(open(@cardset_url))
-    html.css("grid.tarot-list.page").each do |card|
+    html.css("grid.tarot-list.page").each do |cardset|
       cardset_hash = {
-        set: card.css("h2.text-center").text,
-        card: card.css("h3").text,
-        card_url: card.css("a").attribute("href").value}
+        set: card.css(''),
+        card_url: "https://labyrinthos.co/blogs/tarot-card-meanings-list#{card.css('a').attr('href').value}"}
       cards << cardset
     end
   cards 
@@ -30,8 +29,9 @@ class Tarottogo::Scraper
    url = Cardset.select_by_name(input)
    card_array = []
    html = Nokogiri::HTML(open(url))
-   card_hash
-   card[:meaning] = html.css("div.rte.rte--indented-images").text
+   card_hash = {}
+   
+   card_hash[:meaning] = html.css("div.rte.rte--indented-images").text
  end 
 
 end

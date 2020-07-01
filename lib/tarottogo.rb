0ae52b99fcca7cad571c48783e762ae193deb0a6
Jenise Ruehle <jela07@gmail.com>
2020-07-01 16:18:21 +0000

    updates

--- lib/tarottogo.rb
+++ lib/tarottogo.rb
@@ -1,4 +1,4 @@
 require_relative "./tarottogo/version"
-require_relative "./tarottogo/cardset"
+require_relative "./tarottogo/tarot"
 require_relative "./tarottogo/cli"
 require_relative "./tarottogo/scraper"
